0f5473cc00ef0bd94bfa0955df045793bad1ab63
Arden Rasmussen <ardenisthebest@gmail.com>
2019-01-17 14:09:08 -0800

    Added client

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var async = require('async');
 var passport= require('passport');
 var localStrategy = require('passport-local').Strategy;
 
