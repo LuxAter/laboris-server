67df1808807a58b8becca0968a71dedad21f3329
Arden Rasmussen <ardenisthebest@gmail.com>
2019-08-10 23:57:08 -0700

    Added global report

--- routes/api.js
+++ routes/api.js
@@ -109,52 +109,69 @@ router.get("/find", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  req.app.db
-    .get("tasks")
-    .push(
-      _.defaults(
-        {
-          _id: req.body.id,
-          hidden: req.body.hidden,
-          title: req.body.title,
-          parents: req.body.parents ? _.castArray(req.body.parents) : undefined,
-          children: req.body.children
-            ? _.castArray(req.body.children)
-            : undefined,
-          priority: req.body.priority,
-          status: req.body.status,
-          tags: req.body.tags ? _.castArray(req.body.tags) : undefined,
-          entryDate: req.body.entryDate,
-          dueDate: req.body.dueDate,
-          doneDate: req.body.doneDate,
-          modifiedDate: req.body.time ? req.body.time : req.body.modifiedDate,
-          times: req.body.times ? _.castArray(req.body.times) : undefined
-        },
-        {
-          _id: uuidv3(req.body.title + _.now().toString(), uuidv3.URL),
-          hidden: false,
-          parents: [],
-          children: [],
-          priority: 5,
-          status: "active",
-          tags: [],
-          entryDate: _.now(),
-          dueDate: null,
-          doneDate: null,
-          modifiedDate: _.now(),
-          times: []
-        }
+  if (_.isNil(req.body.title)) {
+    res.json({ error: "Title must be defined for a new task", body: req.body });
+  } else {
+    req.app.db
+      .get("tasks")
+      .push(
+        _.defaults(
+          {
+            _id: req.body.id,
+            hidden: req.body.hidden,
+            title: req.body.title,
+            parents: req.body.parents
+              ? _.castArray(req.body.parents)
+              : undefined,
+            children: req.body.children
+              ? _.castArray(req.body.children)
+              : undefined,
+            priority: req.body.priority
+              ? _.asInteger(req.body.priority)
+              : undefined,
+            status: req.body.status,
+            tags: req.body.tags ? _.castArray(req.body.tags) : undefined,
+            entryDate: req.body.entryDate
+              ? _.asInteger(req.body.entryDate)
+              : undefined,
+            dueDate: req.body.dueDate
+              ? _.asInteger(req.body.dueDate)
+              : undefined,
+            doneDate: req.body.doneDate
+              ? _.asInteger(req.body.doneDate)
+              : undefined,
+            times: req.body.times ? _.castArray(req.body.times) : undefined
+          },
+          {
+            _id: uuidv3(req.body.title + _.now().toString(), uuidv3.URL),
+            hidden: false,
+            parents: [],
+            children: [],
+            priority: 5,
+            status: "active",
+            tags: [],
+            entryDate: _.now(),
+            dueDate: null,
+            doneDate: null,
+            modifiedDate: _.now(),
+            times: []
+          }
+        )
       )
-    )
-    .write()
-    .then(entry => res.json(entry));
+      .write()
+      .then(entry => res.json(entry));
+  }
 });
 
 router.post("/stop", (req, res) => {
   req.app.db
     .get("tasks")
     .filter(entry => {
-      return entry.times.length !== 0 && _.last(entry.times).length === 1;
+      return (
+        entry.times.length !== 0 &&
+        _.last(entry.times).length === 1 &&
+        entry.status === "active"
+      );
     })
     .each(entry => {
       entry.times = [
@@ -167,22 +184,23 @@ router.post("/stop", (req, res) => {
     .then(entries => res.json(entries));
 });
 
-router.post("/start/:uuid", (req, res) => {
+router.post("/start/:id", (req, res) => {
   var task = req.app.db.get("tasks").find({
-    _id: req.params.uuid
+    _id: req.params.id,
+    status: "active"
   });
   if (task.value() === undefined) {
     res.json({
-      error: "UUID not found",
-      uuid: req.params.uuid
+      error: "ID not found",
+      id: req.params.id
     });
   } else if (
     task.value().times.length !== 0 &&
     _.last(task.value().times).length === 1
   ) {
     res.json({
-      error: "UUID is already active",
-      uuid: req.params.uuid
+      error: "ID is already active",
+      id: req.params.id
     });
   } else {
     task
@@ -198,22 +216,23 @@ router.post("/start/:uuid", (req, res) => {
   }
 });
 
-router.post("/stop/:uuid", (req, res) => {
+router.post("/stop/:id", (req, res) => {
   var task = req.app.db.get("tasks").find({
-    _id: req.params.uuid
+    _id: req.params.id,
+    status: "active"
   });
   if (task.value() === undefined) {
     res.json({
-      error: "UUID not found",
-      uuid: req.params.uuid
+      error: "ID not found",
+      id: req.params.id
     });
   } else if (
     task.value().times.length === 0 ||
     _.last(task.value().times).length !== 1
   ) {
     res.json({
-      error: "UUID is not active",
-      uuid: req.params.uuid
+      error: "ID is not active",
+      id: req.params.id
     });
   } else {
     task
@@ -226,6 +245,148 @@ router.post("/stop/:uuid", (req, res) => {
             req.body.time ? _.toInteger(req.body.time) : _.now()
           ]
         ]
+      })
+      .write()
+      .then(entry => res.json(entry));
+  }
+});
+
+router.post("/deactivate/:id", (req, res) => {
+  var task = req.app.db.get("tasks").find({
+    _id: req.params.id,
+    status: "active"
+  });
+  if (task.value() === undefined) {
+    res.json({
+      error: "ID not found",
+      id: req.params.id
+    });
+  } else if (
+    task.value().times.length !== 0 &&
+    _.last(task.value().times).length === 1
+  ) {
+    task
+      .assign({
+        modifiedDate: _.now(),
+        state: "inactive",
+        times: [
+          ..._.dropRight(task.value().times),
+          [
+            _.last(task.value().times)[0],
+            req.body.time ? _.toInteger(req.body.time) : _.now()
+          ]
+        ]
+      })
+      .write()
+      .then(entry => res.json(entry));
+  } else {
+    task
+      .assign({
+        modifiedDate: _.now(),
+        state: "inactive"
+      })
+      .write()
+      .then(entry => res.json(entry));
+  }
+});
+
+router.post("/activate/:id", (req, res) => {
+  var task = req.app.db.get("tasks").find({
+    _id: req.params.id,
+    status: "inactive"
+  });
+  if (task.value() === undefined) {
+    res.json({
+      error: "ID not found",
+      id: req.params.id
+    });
+  } else {
+    task
+      .assign({
+        modifiedDate: _.now(),
+        state: "active"
+      })
+      .write()
+      .then(entry => res.json(entry));
+  }
+});
+
+router.get("/tags", (req, res) => {
+  res.json(
+    _.uniq(
+      _.flattenDeep(
+        req.app.db
+          .get("tasks")
+          .map("tags")
+          .value()
+      )
+    )
+  );
+});
+
+router.get("/times", (req, res) => {
+  res.json(
+    _.sum(
+      req.app.db
+        .get("tasks")
+        .map(entry =>
+          _.sumBy(entry.times, ev => {
+            if (ev.length === 1) {
+              return _.now() - ev[0];
+            } else {
+              return ev[1] - ev[0];
+            }
+          })
+        )
+        .value()
+    )
+  );
+});
+
+router.get("/:id", (req, res) => {
+  var task = req.app.db.get("tasks").find({
+    _id: req.params.id
+  });
+  if (task.value() === undefined) {
+    res.json({
+      error: "ID not found",
+      id: req.params.id
+    });
+  } else {
+    res.json(task.value());
+  }
+});
+
+router.post("/:id", (req, res) => {
+  var task = req.app.db.get("tasks").find({
+    _id: req.params.id
+  });
+  const val = task.value();
+  if (val === undefined) {
+    res.json({
+      error: "ID not found",
+      id: req.params.id
+    });
+  } else {
+    task
+      .assign({
+        hidden: req.body.hidden ? req.body.hidden : val.hidden,
+        title: req.body.title ? req.body.title : val.title,
+        parents: req.body.parents ? _.castArray(req.body.parents) : val.parents,
+        children: req.body.children
+          ? _.castArray(req.body.children)
+          : val.children,
+        priority: req.body.priority
+          ? _.asInteger(req.body.priority)
+          : val.priority,
+        status: req.body.status ? req.body.status : val.priority,
+        tags: req.body.tags ? _.castArray(req.body.tags) : val.tags,
+        dueDate: req.body.dueDate ? _.asInteger(req.body.dueDate) : val.dueDate,
+        doneDate: req.body.doneDate
+          ? _.asInteger(req.body.doneDate)
+          : val.doneDate,
+        modifiedDate: _.now(),
+        times: req.body.times ? _.castArray(req.body.times) : val.times
       })
       .write()
       .then(entry => res.json(entry));
